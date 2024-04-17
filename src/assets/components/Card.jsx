3bd9272a0d9223c007612ddf0e23aeee5872c4c0
Gaurav Sharma <gauravsharmatheofficial@gmail.com>
2024-04-17 16:46:19 +0530

    project v1 completed

--- src/assets/components/Card.jsx
+++ src/assets/components/Card.jsx
@@ -1,14 +1,43 @@
 import React from "react";
 import { FaRegFileLines } from "react-icons/fa6";
+import { MdOutlineFileDownload } from "react-icons/md";
+import { IoClose } from "react-icons/io5";
+import { motion } from "framer-motion";
 
-function Card() {
+function Card({ data, reference }) {
   return (
-    <div className="w-60 h-72 bg-zinc-900/90 rounded-[20px] text-white p-5">
+    <motion.div
+      drag
+      dragConstraints={reference}
+      whileDrag={{ scale: 1.2 }}
+      className="relative flex-shrink-0 w-60 h-72 bg-zinc-900/90 rounded-[20px] text-white p-5 overflow-hidden"
+    >
       <FaRegFileLines />
-      <p className="text-sm  leading-tight mt-5 font-semibold">
-        Lorem ipsum, dolor sit amet consectetur adipisicing.
-      </p>
-    </div>
+      <p className="text-sm  leading-tight mt-5 font-semibold">{data.desc}</p>
+
+      <div className="  footer absolute bottom-0  w-full   left-0">
+        <div className="flex items-center justify-between py-3 px-5 mb-3">
+          <h5>{data.fileSize}</h5>
+          <span className="w-6 h-6 bg-zinc-600  rounded-full flex items-center justify-center">
+            {data.close ? (
+              <IoClose />
+            ) : (
+              <MdOutlineFileDownload size="1em" color="#fff" />
+            )}
+          </span>
+        </div>
+
+        {data.tag.isOpen && (
+          <div
+            className={`tag w-full py-4 ${
+              data.tag.tagColor === "blue" ? "bg-blue-600" : "bg-green-600"
+            } flex items-center justify-center`}
+          >
+            <h3 className="text-sm font-semibold ">{data.tag.tagTitle}</h3>
+          </div>
+        )}
+      </div>
+    </motion.div>
   );
 }
 
--- src/assets/components/Foreground.jsx
+++ src/assets/components/Foreground.jsx
@@ -1,10 +1,78 @@
-import React from "react";
+import React, { useRef } from "react";
 import Card from "./Card";
 
 function Foreground() {
+  const ref = useRef(null);
+  const data = [
+    {
+      desc: "This is a description",
+      fileSize: "0.9mb",
+      close: false,
+      tag: {
+        isOpen: true,
+        tagTitle: "Download Now",
+        tagColor: "grean",
+      },
+    },
+    {
+      desc: "This is a description",
+      fileSize: "0.9mb",
+      close: false,
+      tag: {
+        isOpen: true,
+        tagTitle: "Upload",
+        tagColor: "blue",
+      },
+    },
+    {
+      desc: "This is a description",
+      fileSize: "0.9mb",
+      close: false,
+      tag: {
+        isOpen: true,
+        tagTitle: "Download Now",
+        tagColor: "green",
+      },
+    },
+    {
+      desc: "This is a description",
+      fileSize: "0.9mb",
+      close: false,
+      tag: {
+        isOpen: false,
+        tagTitle: "Download Now",
+        tagColor: "green",
+      },
+    },
+    {
+      desc: "This is a description",
+      fileSize: "0.9mb",
+      close: false,
+      tag: {
+        isOpen: true,
+        tagTitle: "Download Now",
+        tagColor: "green",
+      },
+    },
+    {
+      desc: "This is a description",
+      fileSize: "0.9mb",
+      close: false,
+      tag: {
+        isOpen: true,
+        tagTitle: "Download Now",
+        tagColor: "green",
+      },
+    },
+  ];
   return (
-    <div className="fixed top-0 left-0 z-[3] w-full h-full">
-      <Card />
+    <div
+      ref={ref}
+      className="fixed top-0 left-0 z-[3] w-full h-full flex gap-5 flex-wrap p-5"
+    >
+      {data.map((item, index) => (
+        <Card data={item} key={index} reference={ref} />
+      ))}
     </div>
   );
 }
